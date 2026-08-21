0fa3ad54417553f3ada49322ae40ad240498b751
wenping29 <wenping29@163.com>
2026-08-21 17:16:58 +0800

    优化页面显示404

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -2,11 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import ProtectedRoute from './components/ProtectedRoute'
 import Login from './pages/Login'
+import Menus from './pages/Menus'
 import NotFound from './pages/NotFound'
 import Profile from './pages/Profile'
 import ReviewDetail from './pages/ReviewDetail'
 import Reviews from './pages/Reviews'
 import Roles from './pages/Roles'
+import Traitors from './pages/Traitors'
 import Users from './pages/Users'
 
 export default function App() {
@@ -16,10 +18,12 @@ export default function App() {
       <Route element={<ProtectedRoute />}>
         <Route element={<Layout />}>
           <Route path="/" element={<Navigate to="/reviews" replace />} />
+          <Route path="/traitors" element={<Traitors />} />
           <Route path="/reviews" element={<Reviews />} />
           <Route path="/reviews/:rid" element={<ReviewDetail />} />
           <Route path="/users" element={<Users />} />
           <Route path="/roles" element={<Roles />} />
+          <Route path="/menus" element={<Menus />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
         </Route>
