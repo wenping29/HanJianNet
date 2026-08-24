dc44b4ee0d1045971a0502c870400dd80265fa85
wenping29 <wenping29@163.com>
2026-08-24 17:34:04 +0800

    阿松大

--- web/src/style/index.ts
+++ web/src/style/index.ts
@@ -3,18 +3,13 @@
     paddingTop: "1rem",
     paddingBottom: "1rem"
   }
-
   const headerMenuItemStyle = {
     width: '100%',
     minWidth: '80px',
   }
-
   const footerContainerPageStyle = {
    padding: '0.5rem'
   }
-
-
-
   export {
     containerPageStyle,
     headerMenuItemStyle,
