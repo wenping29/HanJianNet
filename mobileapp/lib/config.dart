2a6c9bce4375b43058d3512514b950ff548bc54f
wenping29 <wenping29@163.com>
2026-09-04 23:27:35 +0800

    asd

--- mobileapp/lib/config.dart
+++ mobileapp/lib/config.dart
@@ -1,9 +1,15 @@
-import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart';
 
 /// 默认 API 地址：按平台区分。
-/// Android 模拟器访问宿主机用 10.0.2.2；iOS 模拟器用 127.0.0.1。
+/// Web 使用 localhost；Android 模拟器用 10.0.2.2；iOS 模拟器用 127.0.0.1。
 String get kDefaultApiBaseUrl {
-  if (Platform.isAndroid) return 'http://10.0.2.2:3000';
-  if (Platform.isIOS) return 'http://127.0.0.1:3000';
-  return 'http://localhost:3000';
+  if (kIsWeb) return 'http://localhost:3000';
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.android:
+      return 'http://10.0.2.2:3000';
+    case TargetPlatform.iOS:
+      return 'http://127.0.0.1:3000';
+    default:
+      return 'http://localhost:3000';
+  }
 }
