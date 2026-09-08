6b656e835a3270d880d43ad4a0103cb4c9f8d69a
wenping29 <wenping29@163.com>
2026-09-08 15:18:45 +0800

    测试prd打包能否触发

--- mobileapp/lib/main.dart
+++ mobileapp/lib/main.dart
@@ -10,6 +10,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Session.instance.load();
   runApp(const HanJianApp());
+  // await Session.instance.clear();
 }
 
 class HanJianApp extends StatelessWidget {
