1fc50fc4c9a7254f60109f948e3ffa20944ff820
wenping29 <wenping29@163.com>
2026-09-14 09:21:07 +0800

    编辑页面修改为打开新的浏览器页面

--- admin/src/pages/Traitors.tsx
+++ admin/src/pages/Traitors.tsx
@@ -234,7 +234,7 @@ export default function Traitors() {
                           <button
                             type="button"
                             className="btn-ghost !px-3 !py-1.5 text-xs"
-                            onClick={() => navigate(`/traitors/${tr.id}/edit`)}
+                            onClick={() => window.open(`/traitors/${tr.id}/edit`, '_blank')}
                           >
                             {t('traitors.edit')}
                           </button>
