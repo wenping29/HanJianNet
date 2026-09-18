4888dfe8d12257a12c16345d8741e3656497bdba
wenping29 <wenping29@163.com>
2026-09-18 16:39:12 +0800

    更新数据

--- webapi/sql/VisitLogs.mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `VisitLogs` (
-    `Id` bigint NOT NULL AUTO_INCREMENT,
-    `VisitorToken` varchar(128) NOT NULL,
-    `Path` varchar(256) NOT NULL DEFAULT '',
-    `CreatedAt` datetime(6) NOT NULL,
-    CONSTRAINT `PK_VisitLogs` PRIMARY KEY (`Id`)
-);
-CREATE INDEX `IX_VisitLogs_CreatedAt` ON `VisitLogs` (`CreatedAt`);
-CREATE INDEX `IX_VisitLogs_VisitorToken` ON `VisitLogs` (`VisitorToken`);
